3969df0115742e67456a8f466a06e922387a89bc
minmin <>
2021-02-08 17:52:25 +0900

    works layout

--- script.js
+++ script.js
@@ -20,4 +20,36 @@ $(document).ready(function(){
     $('.contact').click(function(){
         $('.popup_contact').fadeIn();
     })
+
+    let slide_num = 0;
+    let slide_page = 1;
+    $('.popup_right_bt').click(function(){
+        if(slide_num > -600){
+        slide_num = slide_num-100;
+        slide_page = slide_page + 1;
+        $('.popup_slide').css({'left': slide_num+'%'});
+        $('.popup_page p').text(slide_page+'/7')
+    }
+        else {
+            slide_num = 0;
+            $('.popup_slide').css({'left':'0'})
+            slide_page = 1;
+            $('.popup_page p').text('1/7')
+        }
+    })
+
+    $('.popup_left_bt').click(function(){
+        if(slide_num < 0){
+        slide_num = slide_num+100;
+        slide_page = slide_page - 1;
+        $('.popup_slide').css({'left': slide_num+'%'});
+        $('.popup_page p').text(slide_page+'/7')
+    }
+        else {
+            slide_num = -600;
+            $('.popup_slide').css({'left':'-600%'})
+            slide_page = 7;
+            $('.popup_page p').text('7/7')
+        }
+    })
 });
